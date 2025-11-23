996e0c376d7f1167819b18b753161c0ed9f24599
kurojs <kuuro.js@gmail.com>
2025-11-23 14:08:29 -0600

    Remove Firefox addon files and clean up repo

--- background.js
+++
@@ -1,8 +0,0 @@
-browser.browserAction.onClicked.addListener(() => {
-    browser.windows.create({
-        url: "pages/index.html",
-        type: "popup",
-        width: 400,
-        height: 600
-    });
-});
